07212e938c10ea6cc4bdcc9484e4b11fec9b62d8
Frédérik Bilhaut <fbp@xineo.net>
2025-01-10 12:30:27 +0100

    return IndexError from TensorsMeta::create_span() instead of panicking

--- src/model/pipeline/mod.rs
+++ src/model/pipeline/mod.rs
@@ -4,32 +4,12 @@ pub mod token;
 pub mod span;
 
 use ort::session::{SessionInputs, SessionOutputs};
-use crate::util::result::Result;
+use crate::util::{error::IndexError, result::Result};
 use crate::util::compose::Composable;
 use crate::text::span::Span;
 use crate::text::token::Token;
 use super::{input::text::TextInput, output::decoded::SpanOutput, params::Parameters};
 
-
-/// Data to be transmitted, beside the tensors themselves, from pre-processing to post-processing
-pub struct TensorsMeta {
-    pub texts: Vec<String>,
-    pub tokens: Vec<Vec<Token>>,
-    pub entities: Vec<String>,
-    pub num_words: usize,
-}
-
-impl TensorsMeta {
-    /// Creates a span given the necessary indexes and the known meta data.
-    /// Currently panics if the offsets are not correct (might return an error in the future)
-    pub fn create_span(&self, sequence_id: usize, start_token: usize, end_token: usize, class: usize, probability: f32) -> Result<Span> {
-        let start_offset = self.tokens.get(sequence_id).unwrap().get(start_token).unwrap().start();
-        let end_offset = self.tokens.get(sequence_id).unwrap().get(end_token).unwrap().end();
-        let text = self.texts.get(sequence_id).unwrap()[start_offset..end_offset].to_string();
-        let class = self.entities.get(class).unwrap().clone();
-        Ok(Span::new(sequence_id, start_offset, end_offset, text, class, probability))
-    }
-}
 
 /// Defines a generic pre-processor
 pub trait PreProcessor<'a>: Composable<TextInput, (SessionInputs<'a, 'a>, TensorsMeta)> { }
@@ -47,3 +27,35 @@ pub trait Pipeline<'a> {
     fn post_processor(&self, params: &Parameters) -> impl PostProcessor<'a>;
 }
 
+
+
+/// Data to be transmitted, beside the tensors themselves, from pre-processing to post-processing
+pub struct TensorsMeta {
+    pub texts: Vec<String>,
+    pub tokens: Vec<Vec<Token>>,
+    pub entities: Vec<String>,
+    pub num_words: usize,
+}
+
+impl TensorsMeta {
+    /// Creates a span given the necessary indexes and the tensor meta data.
+    /// Currently panics if the offsets are not correct (might return an error in the future)
+    pub fn create_span(&self, sequence_id: usize, start_token: usize, end_token: usize, class: usize, probability: f32) -> Result<Span> {
+        let sequence = self.tokens.get(sequence_id).ok_or(IndexError::new("meta.tokens", sequence_id))?;
+        let start_token = sequence.get(start_token).ok_or(IndexError::new("meta.tokens[]", start_token))?;
+        let start_offset = start_token.start();
+        let end_token = sequence.get(end_token).ok_or(IndexError::new("meta.tokens[]", end_token))?;
+        let end_offset = end_token.end();
+        let text = &self.texts.get(sequence_id).ok_or(IndexError::new("meta.texts", sequence_id))?;
+        let text = text[start_offset..end_offset].to_string();
+        let class = self.entities.get(class).ok_or(IndexError::new("meta.entities", class))?.to_string();
+        Ok(Span::new(
+            sequence_id, 
+            start_offset, 
+            end_offset,
+            text, 
+            class, 
+            probability)
+        )
+    }
+}
