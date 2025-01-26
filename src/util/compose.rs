f3d1c46eaa0ed3b452f4bec24a567f8cf6b5a544
Frédérik Bilhaut <fbp@xineo.net>
2025-01-26 23:39:58 +0100

    allow for spcial composition of composables that accept or return tuples

--- src/util/compose.rs
+++ src/util/compose.rs
@@ -16,7 +16,7 @@
 //! 
 //! struct AddTo { addend: usize }
 //! 
-//! impl Composable<usize,usize> for AddTo {
+//! impl Composable<usize, usize> for AddTo {
 //!     fn apply(&self, input: usize) -> Result<usize> { Ok(input + self.addend) }
 //! }
 //! 
@@ -28,7 +28,37 @@
 //!     assert_eq!(result, 9);
 //!     Ok(())
 //! }
+//! ``````
+//! 
+//! It is also possible to combine a composable of type `X->(Y, V1)` with another 
+//! composable of type `Y->(Z, V2)` into a composable of type `X->(Z, (V1, V2))`.
+//! This operation is provided by `compose_t()` and `composed_r!`.
+//! 
+//! For example:
+//! 
 //! ```
+//! use gliner::util::result::Result;
+//! use gliner::util::compose::*;
+//! 
+//! struct AddToMsg { addend: usize }
+//! 
+//! impl Composable<usize, (usize, String)> for AddToMsg {
+//!     fn apply(&self, input: usize) -> Result<(usize, String)> { Ok((input + self.addend, "hello".to_string())) }
+//! }
+//! 
+//! fn demo_t() -> Result<()> {
+//!     let increment = AddToMsg { addend: 1 };
+//!     let square = |x: usize| Ok(((x * x), "world".to_string()));
+//!     let composition = compose_t(increment, square);
+//!     let (result, (msg1, msg2)) = composition.apply(2)?;
+//!     assert_eq!(result, 9);
+//!     assert_eq!(msg1, "hello");
+//!     assert_eq!(msg2, "world");
+//!     Ok(())
+//! }
+//! ```
+//! 
+//! The reverse operation is provided by `compose_rt()` and `composed_rt!`.
 
 use std::{fmt::Display, marker::PhantomData};
 use super::result::Result;
@@ -86,6 +116,81 @@ impl<T1, T2, I, O, X> Composable<I, O> for Composed<T1, T2, X> where
     }
 }
 
+/// Special composition that combines tuple values
+/// 
+/// Example: 
+/// * Composable: `X->(Y, V1)`
+/// * And composable: `Y->(Z, V2)`
+/// * Are composed into: `X->(Z, (V1, V2))`
+pub fn compose_t<T1, T2, I, O, V1, V2, X>(t1: T1, t2: T2) -> impl Composable<I, (O, (V1, V2))> where
+    T1: Composable<I, (X, V1)>,
+    T2: Composable<X, (O, V2)>,
+{
+    ComposedTuples::new(t1, t2)
+}
+
+struct ComposedTuples<T1, T2, X> {
+    t1: T1,
+    t2: T2,
+    _marker: PhantomData<X>,
+}
+
+impl<T1, T2, X> ComposedTuples<T1, T2, X> {
+    pub fn new(t1: T1, t2: T2) -> Self {
+        Self { t1, t2, _marker: PhantomData }
+    }
+}
+
+impl<T1, T2, I, O, V1, V2, X> Composable<I, (O, (V1, V2))> for ComposedTuples<T1, T2, X> where 
+    T1: Composable<I, (X, V1)>,
+    T2: Composable<X, (O, V2)>,
+{
+    fn apply(&self, input: I) -> Result<(O, (V1, V2))> {
+        let (x, v1) = self.t1.apply(input)?;
+        let (o, v2) = self.t2.apply(x)?;
+        Ok((o, (v1, v2)))
+    }
+}
+
+
+/// Special composition that combines tuple values
+/// 
+/// Example: 
+/// * Composable: `(X, V1)->Y`
+/// * And composable: `(Y, V2)->Z`
+/// * Are composed into: `(X, (V1, V2))->Z`
+pub fn compose_rt<T1, T2, I, O, V1, V2, X>(t1: T1, t2: T2) -> impl Composable<(I, (V2, V1)), O> where
+    T1: Composable<(I, V1), X>,
+    T2: Composable<(X, V2), O>,
+{
+    ComposedTuplesR::new(t1, t2)
+}
+
+struct ComposedTuplesR<T1, T2, X> {
+    t1: T1,
+    t2: T2,
+    _marker: PhantomData<X>,
+}
+
+impl<T1, T2, X> ComposedTuplesR<T1, T2, X> {
+    pub fn new(t1: T1, t2: T2) -> Self {
+        Self { t1, t2, _marker: PhantomData }
+    }
+}
+
+impl<T1, T2, I, O, V1, V2, X> Composable<(I, (V2, V1)), O> for ComposedTuplesR<T1, T2, X> where 
+    T1: Composable<(I, V1), X>,
+    T2: Composable<(X, V2), O>,
+{
+    fn apply(&self, input: (I, (V2, V1))) -> Result<O> {
+        let (i, (v2, v1)) = input;
+        let x = self.t1.apply((i, v1))?;
+        let o = self.t2.apply((x, v2))?;
+        Ok(o)
+    }
+}
+
+
 
 /// Utility `Composable` that prints the passed value and returns it untouched
 pub struct Print {
@@ -134,4 +239,42 @@ macro_rules! composed {
 
 }
 
+
+/// Macro-rules for easy composition (with output tuples)
+#[macro_export]
+macro_rules! composed_t {
+
+    ($e:expr) => {{
+        $e
+    }};
+
+    ($e:expr, $($es:expr),+ ) => {{
+        let t1 = $crate::composed_t! { $e };
+        let t2 = $crate::composed_t! { $($es),+ };
+        $crate::util::compose::compose_t(t1, t2)
+    }};
+
+}
+
+
+
+
+/// Macro-rules for easy composition (with input tuples)
+#[macro_export]
+macro_rules! composed_rt {
+
+    ($e:expr) => {{
+        $e
+    }};
+
+    ($e:expr, $($es:expr),+ ) => {{
+        let t1 = $crate::composed_rt! { $e };
+        let t2 = $crate::composed_rt! { $($es),+ };
+        $crate::util::compose::compose_rt(t1, t2)
+    }};
+
+}
+
 pub(crate) use composed;
+pub(crate) use composed_t;
+pub(crate) use composed_rt;
